83c22ec4fef33e1c6c746d489a0b49e1acc9ed3a
Hyperzsb <hyperzsb@qq.com>
2020-07-18 22:22:09 +0800

    [feat]Update jenkins/build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "Executing build.sh..."
-echo "Start building: "
-docker run --rm -v /home/admin/github/agreement-recognizer-ui:/usr/src/app -w /usr/src/app node:latest sh -c "npm install && npm run build"
-echo "Building Completed!"
-echo "Exit build.sh"
-
+echo " - Executing build.sh..."
+work_directory="/home/admin/github/agreement-recognizer-ui"
+echo "- Work directory is: $work_directory"
+echo "- Start building..."
+if docker run --rm -v $work_directory:/usr/src/app -w /usr/src/app node:latest sh -c "npm install && npm run build"; then
+  echo " - Building completed!"
+else
+  echo " - Building failed!"
+fi
+echo " - Exit build.sh"
